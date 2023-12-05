3d2d267338c363e6ca21d256555e25c0d9bc0f6f
CodeSandbox <support@codesandbox.io>
2023-12-05 21:36:46 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+import "./styles.css";
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+for (let i = 0; i < menuLinks.length; i++) {
+  const a = document.createElement("a");
+  a.setAttribute("href", menuLinks[i].href);
+  a.innerText = menuLinks[i].text;
+  topMenuEl.append(a);
+}
